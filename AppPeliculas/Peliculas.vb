f6d80ae70443e70b9b9f9546e9c50fbe4e247411
ikerisasi <iker_isasian@elorrieta-errekamari.com>
2022-01-18 18:51:39 +0100

    añadidas clases usuarios y peliculas

---
+++ AppPeliculas/Peliculas.vb
@@ -0,0 +1,43 @@
+﻿Public Class Peliculas
+    ' una prueba desde VS 
+    Private _titulo As String
+    Public Property titulo() As String
+        Get
+            Return _titulo
+        End Get
+        Set(ByVal value As String)
+            _titulo = value
+        End Set
+    End Property
+
+    Private _anio As String
+    Public Property Anio() As String
+        Get
+            Return _anio
+        End Get
+        Set(ByVal value As String)
+            _anio = value
+        End Set
+    End Property
+
+    Private _director As String
+    Public Property director() As String
+        Get
+            Return _director
+        End Get
+        Set(ByVal value As String)
+            _director = value
+        End Set
+    End Property
+
+    Private _genero As String
+    Public Property genero() As String
+        Get
+            Return _genero
+        End Get
+        Set(ByVal value As String)
+            _genero = value
+        End Set
+    End Property
+
+End Class
---
+++ AppPeliculas/Usuarios.vb
@@ -0,0 +1,21 @@
+﻿Public Class Usuario
+    Private _user As String
+    Public Property user() As String
+        Get
+            Return _user
+        End Get
+        Set(ByVal value As String)
+            _user = value
+        End Set
+    End Property
+
+    Private _pass As String
+    Public Property pass() As String
+        Get
+            Return _pass
+        End Get
+        Set(ByVal value As String)
+            _pass = value
+        End Set
+    End Property
+End Class
